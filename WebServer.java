b3dae6a73e583a8bb2681c2bace23c5badc89459
NQureshi850 <nameer.qureshi@gmail.com>
2017-12-04 22:57:29 -0500

    error codes

--- WebServer.java
+++ WebServer.java
@@ -22,70 +22,94 @@ public class WebServer {
 	static int port = 5001;
 	private static Firebase base = null;
 	private static FirebaseResponse response = null;
-	
+
 	static class getUserHandler implements HttpHandler 
 	{
 		public void handle(HttpExchange he) throws IOException 
 		{
-            Map<String, Object> parameters = new HashMap<String, Object>();
-            InputStreamReader isr = new InputStreamReader(he.getRequestBody(), "utf-8");
-            BufferedReader br = new BufferedReader(isr);
-            String query = br.readLine();
-            parseBody(query, parameters);
-            
-            String httpResponse = "";
-            for (String key : parameters.keySet())
-            	httpResponse += key /*+ " = " + parameters.get(key)*/ + "\n";
-            he.sendResponseHeaders(200, httpResponse.length());
-            OutputStream os = he.getResponseBody();
-            os.write(httpResponse.toString().getBytes());
-            os.close();
+			Map<String, Object> parameters = new HashMap<String, Object>();
+			InputStreamReader isr = new InputStreamReader(he.getRequestBody(), "utf-8");
+			BufferedReader br = new BufferedReader(isr);
+			String query = br.readLine();
+			int code = parseBody(query, parameters);
+
+			if(code == 0)
+			{
+				String httpResponse = "You suck";
+				he.sendResponseHeaders(400, httpResponse.length());
+				OutputStream os = he.getResponseBody();
+				os.write(httpResponse.toString().getBytes());
+				os.close();
+			}
+
+			else
+			{
+				String httpResponse = "";
+				for (String key : parameters.keySet())
+					httpResponse += key /*+ " = " + parameters.get(key)*/ + "\n";
+				he.sendResponseHeaders(200, httpResponse.length());
+				OutputStream os = he.getResponseBody();
+				os.write(httpResponse.toString().getBytes());
+				os.close();
+			}
 
 		}
 
 	}
-	
+
 	static class updateScoreHandler implements HttpHandler
 	{
 		public void handle(HttpExchange he) throws IOException 
 		{
-            Map<String, Object> parameters = new HashMap<String, Object>();
-            InputStreamReader isr = new InputStreamReader(he.getRequestBody(), "utf-8");
-            BufferedReader br = new BufferedReader(isr);
-            String query = br.readLine();
-            parseBody(query, parameters);
-            
-            String httpResponse = "";
-            for (String key : parameters.keySet())
-            	httpResponse += key + /*" = " + parameters.get(key) + */"\n";
-            he.sendResponseHeaders(200, httpResponse.length());
-            OutputStream os = he.getResponseBody();
-            os.write(httpResponse.toString().getBytes());
-            os.close();
-			
+			Map<String, Object> parameters = new HashMap<String, Object>();
+			InputStreamReader isr = new InputStreamReader(he.getRequestBody(), "utf-8");
+			BufferedReader br = new BufferedReader(isr);
+			String query = br.readLine();
+			int code = parseBody(query, parameters);
+
+			if(code == 0)
+			{
+				String httpResponse = "You suck";
+				he.sendResponseHeaders(400, httpResponse.length());
+				OutputStream os = he.getResponseBody();
+				os.write(httpResponse.toString().getBytes());
+				os.close();
+			}
+
+			else
+			{
+				String httpResponse = "";
+				for (String key : parameters.keySet())
+					httpResponse += key + /*" = " + parameters.get(key) + */"\n";
+				he.sendResponseHeaders(200, httpResponse.length());
+				OutputStream os = he.getResponseBody();
+				os.write(httpResponse.toString().getBytes());
+				os.close();
+			}
+
 		} 
 	}
-	
+
 	static class HomePageHandler implements HttpHandler
 	{
 		public void handle(HttpExchange he) throws IOException 
 		{
-            String homepage = "<h1> Test Page </h1>";
-            he.sendResponseHeaders(200, homepage.length());
-            OutputStream os = he.getResponseBody();
-            os.write(homepage.getBytes());
-            os.close();
-			
+			String homepage = "<h1> Test Page </h1>";
+			he.sendResponseHeaders(200, homepage.length());
+			OutputStream os = he.getResponseBody();
+			os.write(homepage.getBytes());
+			os.close();
+
 		} 
 	}
-	
+
 	public static int parseBody(String query, Map<String, Object> parameters) throws UnsupportedEncodingException
 	{
 		if(query == null)
 		{
 			return 0;
 		}
-		
+
 		else
 		{
 			String pairs[] = query.split("&");
@@ -94,24 +118,24 @@ public class WebServer {
 				String param[] = pair.split("=");
 				String key = null;
 				String value = null;
-				
+
 				if(param.length > 0)
 					key = URLDecoder.decode(param[0], System.getProperty("file.encoding"));
-				
+
 				if(param.length > 1)
 					value = URLDecoder.decode(param[1], System.getProperty("file.encoding"));
-				
+
 				if(parameters.containsKey(key))
 				{
 					Object object = parameters.get(key);
-					
+
 					if(object instanceof List<?>)
 					{
 						List<String> values = (List<String>) object;
-						
+
 						values.add(value);
 					}
-					
+
 					else if(object instanceof String)
 					{
 						List<String> values = new ArrayList<String>();
@@ -120,17 +144,17 @@ public class WebServer {
 						parameters.put(key,  values);
 					}
 				}
-				
+
 				else
 				{
 					parameters.put(key, value);
 				}
 			}
-			
+
 			return 1;
 		}
 	}
-	
+
 	public static void main(String[] args) throws FirebaseException, JsonParseException, JsonMappingException, IOException, JacksonUtilityException, HttpException
 	{
 		base = new Firebase("https://cs252-fc67c.firebaseio.com/");
@@ -138,24 +162,24 @@ public class WebServer {
 		//Map<String, Object> dataMap = new LinkedHashMap<String, Object>();
 		//dataMap.put( "test1", "10" );
 		//base.put("test2/test3", dataMap);
-		
+
 		/*DefaultBHttpServerConnection connection = new DefaultBHttpServerConnection(32406);
 		connection.bind(server.accept());
 		HttpRequest request = connection.receiveRequestHeader();
 		connection.receiveRequestEntity((HttpEntityEnclosingRequest)request);
 		HttpEntity entity = ((HttpEntityEnclosingRequest)request).getEntity();
 		System.out.println(EntityUtils.toString(entity));
-		*/
-		
+		 */
+
 		HttpServer server = HttpServer.create(new InetSocketAddress(port), 0);
 		server.createContext("/", new HomePageHandler());
 		server.createContext("/getUser", new getUserHandler());
 		server.createContext("/updateScore", new updateScoreHandler());
 		server.setExecutor(null);
 		server.start();
-		
+
 		/*ServerSocket server = new ServerSocket(port);
-		
+
 		while(true)
 		{
 			Socket clientRequest = server.accept();
@@ -163,16 +187,16 @@ public class WebServer {
 			PrintWriter writer = new PrintWriter(clientRequest.getOutputStream());
 			BufferedReader reader = new BufferedReader(new InputStreamReader(clientRequest.getInputStream()));
 			String line = null;
-			
+
 			writer.println("hello");
-			
+
 			while((line = reader.readLine()) != null)
 			{
 				System.out.println(line);
 
 			}
 		}
-		*/
-		
+		 */
+
 	}
 }
